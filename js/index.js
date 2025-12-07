c0a45d5ed902d31967ca398b4b2f6063cc99dc28
Anonimous1425845 <dominik.pysny@outlook.sk>
2025-12-07 11:01:10 +0100

    index.html -> home.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 alert("Toto je testvacia a zaroveň prvá stránka.")
 alert("Môže obsahovať copyright")
 alert("Použiť iba na nekomerčné použiťie")
+
+function redirrect(url) {
+    window.location.href = url
+}
+
+redirrect("home.html")
