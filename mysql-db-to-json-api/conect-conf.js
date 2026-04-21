5a88316b1e9973c9da9b46e00de8b458ddc9d1ce
Anonimous1425845 <dominik.pysny@outlook.sk>
2026-04-21 21:45:39 +0200

    UM it is dumb

--- mysql-db-to-json-api/conect-conf.js
+++
@@ -1,9 +0,0 @@
-const TheWay = {
-        host: 'localhost',
-        user: 'web',
-        password: 'mASteRKey3346632DUKLOCKPLUssALeCiaO',
-        database: 'sudobase'
-    };
-
-// Exportujeme funkciu, aby bola viditeľná vonku
-module.exports = TheWay;
