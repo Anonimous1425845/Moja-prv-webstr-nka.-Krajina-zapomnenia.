f583a13e9f89ff68bc2736232207d76ac2baf3b1
Anonimous1425845 <dominik.pysny@outlook.sk>
2026-06-19 18:16:46 +0200

    chat

---
+++ chat/api/main.js
@@ -0,0 +1,119 @@
+// Websocket chat api to avoid dirrect connection and IP Fishing
+// Runs on 4000 and uses json to comunicate
+const wss = require('ws');
+const formatedDate = require('../../js/lil/date');
+const WS = new wss.Server({ port: 4000 });
+
+// connection
+WS.on('connection', (ws, req) => {
+    //const idr = Math.floor(Math.random * 100000000) + 1;
+    //let id = `ID: ${idr}`;
+    let id = req.socket.remoteAddress
+    console.log(`[Connector] Someonee connected from: ${id}.`);
+    // reciver
+    ws.on('message', (messageraw) => {
+        try {
+            const message = messageraw.toString();
+            const data = JSON.parse(message);
+
+            if (data.type === 'ping') {
+                if (data.ping === 'ping') {
+                    ws.send(
+                        JSON.stringify({
+                            type: 'pong',
+                            pong: 'pong',
+                            time: formatedDate()
+                        })
+                    );
+                };
+                if (data.ping === 'health') {
+                    ws.send(
+                        JSON.stringify({
+                            online: true,
+                            time: formatedDate()
+                        })
+                    );
+                };
+            };
+
+            if (data.type === 'setnick') {
+                // setter
+                ws.nickname = data.nick;
+                id = data.nick;
+                
+                // Welcome to the internet!
+                WS.clients.forEach((client) => {
+                    if (client.readyState === 1) {
+                        // sender-all joined
+                        client.send(JSON.stringify({
+                            type: 'sobytiye',
+                            msg: `${id} Joined the chat!`,
+                            time: formatedDate()
+                        }));
+                    }
+                });
+
+                // sender
+                ws.send(JSON.stringify({
+                    type: 'response',
+                    msg: 'Nickname has been set to ' + ws.nickname + '. And you are inside chat now!',
+                    time: formatedDate()
+                }));
+            }
+
+            // Only handle real chat messages (type === 'msg').
+            // Avoid treating other message types (like 'setnick') as chat messages,
+            // which previously caused broadcasts with `msg: undefined`.
+            if (data.type === 'msg') {
+                const nickToUse = ws.nickname || id;
+                // GPT-5 mini helped fix this bug
+                const text = typeof data.msg === 'undefined' ? '' : data.msg;
+                console.log(`[${nickToUse}] ${text}`);
+                WS.clients.forEach((client) => {
+                    // sender-all
+                    if (client.readyState === 1) {
+                        // sender (master message handler)
+                        client.send(JSON.stringify({
+                            type: 'msg',
+                            nick: nickToUse,
+                            msg: text,
+                            time: formatedDate()
+                        }));
+                    }
+                });
+            }
+        } catch (error) {
+            // womp womp
+            console.log(`[${id}] Got invalid JSON ${error}`);
+            try {   
+                ws.send(JSON.stringify({
+                    type: 'ERROR',
+                    msg: 'You send invalid request'
+                }))
+            } catch (e) {}
+        }
+    })
+    // he left did you seen him?!
+    ws.on('close', () => {
+        console.log(`[${id}] Has left the chat!`)
+        WS.clients.forEach((client) => {
+            if (client.readyState === 1) {
+                // sender-all left
+                client.send(JSON.stringify({
+                    type: 'sobytiye',
+                    msg: `${id} Has left the chat!`,
+                    time: formatedDate()
+                }));
+            }
+        });
+    });
+});
+
+/*
+WS.on('message', (message) => {
+    const data = JSON.prase(message);
+    console.log(data)
+})
+*/
+
+console.log("Runnin' Chat Web Socket API on :4000");;
---
+++ chat/chat.js
@@ -0,0 +1,134 @@
+// client side sender and reciever
+const port = 4000;
+const hostt = window.location.hostname;
+const Nick = new URLSearchParams(window.location.search).get('nick');
+const WS = new WebSocket(`ws://${hostt}:${port}`);
+
+const msgEL = document.getElementById('msg');
+const openEL = document.getElementById('isopen');
+const pingEL = document.getElementById('pingEL');
+const pongEL = document.getElementById('pongEL');
+const mayDontDeleteEL = document.getElementById('mayDontDelete');
+
+function FINEopen(what) {
+    if (what === true) {
+        openEL.style.color = 'rgb(0, 255, 0)';
+        openEL.innerText = 'true';
+        return 0;
+    }
+    if (what === false) {
+        openEL.style.color = 'red';
+        openEL.innerText = 'false';
+        return 0;
+    }
+    console.error("FINEopen didnot catch this parameter " + what);
+    return 1;
+};
+
+function PingSTAT(what) {
+    pingEL.style.color = what;
+    return;
+}
+function PongSTAT(what) {
+    pongEL.style.color = what;
+    return;
+}
+
+function PingPong() {
+    if(pingEL.style.color !== 'blue'){
+        PingSTAT('blue');
+        WS.send(
+            JSON.stringify({
+                type: 'ping',
+                ping: 'ping'
+            })
+        );
+        PongSTAT('blue');
+    } else {
+        PingSTAT('red');
+        FINEopen(false);
+        PongSTAT('red');
+        
+    }
+    return 0;
+};
+
+// for reconnect this to 107 needs to be in function with new WS(..) as well
+WS.onopen = () => {
+    console.log("Connected to WebSocket");
+    PingPong();
+    FINEopen(true);
+    WS.send(JSON.stringify({
+        type: 'setnick',
+        nick: Nick
+    }));
+    console.log('Nick Set');
+}
+
+// Inbound messages processor
+WS.onmessage = (event) => {
+    FINEopen(true);
+    const data = JSON.parse(event.data);
+    if(data.type === 'pong') {
+        PingSTAT('rgb(0, 255, 0)');
+        FINEopen(true);
+        PongSTAT('rgb(0, 255, 0)');
+    };
+
+    if(data.type === 'response') {
+        alert(data.msg + ' ' + data.time);
+        return;
+    };
+
+    if(data.type === 'sobytiye') {
+        msgEL.insertAdjacentHTML("beforeend", `
+            <p>${data.msg} at ${data.time}</p>
+            <p>-------------------</p>
+        `);
+        return;
+    };
+
+    if(data.type === 'msg') {
+        msgEL.insertAdjacentHTML("beforeend", `
+            <div class="messageclass" >
+            <p style="text-align: right; text-decoration: underline;">${data.time}
+            <p>${data.nick} :  ${data.msg}</p>
+            </div>
+            <p>-------------------</p>
+        `);
+        return;
+    };
+
+    if(data.type === 'ERROR') {
+        alert('ERROR: ' + data.msg);
+        console.error(data.msg);
+        return;
+    };
+};
+
+// Outbound Messeges Sender Functions
+function SendMSG(msgIN) {
+    console.log('sending', msgIN)
+    if(WS && WS.readyState === 1) {
+        console.log('sendin')
+        WS.send(JSON.stringify({
+            type: 'msg',
+            msg: msgIN
+        }));
+        FINEopen(true);
+        if(!mayDontDeleteEL.checked){
+            document.getElementById('messageinput').value = '';
+        }
+    } else {
+        console.error('Socket not open');
+        FINEopen(false)
+    }
+};
+
+setInterval(() => {
+    PingPong();
+}, 3000);
+
+// priviat moscow
+// znaajece zlak sje hutory što russia nezprobuje songy 
+// ale oni maju fajne songy debili
