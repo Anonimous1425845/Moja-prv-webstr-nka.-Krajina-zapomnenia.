f02a5fa4a68bf0d0c0d0d0768deae50be77dcaf6
Anonimous1425845 <dominik.pysny@outlook.sk>
2026-07-11 15:54:23 +0200

    stuff

--- mysql-db-to-json-api/main.js
+++ mysql-db-to-json-api/main.js
@@ -1,12 +1,15 @@
 const dbcreadentials = JSON.parse(require('fs').readFileSync('./connect-conf.json', 'utf8'));
-const { "pass": configuredpass } = JSON.parse(require('fs').readFileSync('./pass.json', 'utf8'));
+const { "pass": configuredpass, "pass2": configuredpass2 } = JSON.parse(require('fs').readFileSync('./pass.json', 'utf8'));
 const { "pass": pass } = JSON.parse(require('fs').readFileSync('./pass.json', 'utf8'));
 const formatedDate = require('../js/lil/date');
 const mysql = require('mysql2/promise');
 const express = require('express');
 const cors = require('cors');
+const { table, time } = require('console');
 const app = express();
 const PORT = 3003;
+
+console.log("pass set to: " + configuredpass);
 
 app.use(cors());
 
@@ -56,18 +59,34 @@ app.get('/select',
     }
 );
 
+// BRAIN F*CK
+function AUTH(passUrl,passHead) {
+    // nájde použitý password parameter
+    const pass = passUrl ?? passHead;
+
+    if ([configuredpass, configuredpass2].includes(pass)) {
+        // autorizovaný
+        return true;
+    } else {
+        // GET OUT
+        return false;
+    }
+}
+
 app.get('/insert',
     // Use like this api/insert?t='table'&colums='columtofill,secondone'&values='values representing your colums selection'
     // YOU NEWER NEED TO USE '' IN REQUEST UNLESS VALUES ONE! UNDERSTAND?!
     async (req, res) => {
-        console.log('New Request!');
+        console.log('[Insert] New Request!');
         const passhead = req.headers['pass'];
         const password = req.query.pass;
         const TABLE = req.query.t;
         const COLUMS = req.query.colums;
         const VALUES = req.query.values;
         const Time = formatedDate();
-        if(password === configuredpass || passhead === configuredpass){
+        // if(password === configuredpass || passhead === configuredpass){
+        // Make use of URL Parm possible
+        if (AUTH(password,passhead) === true) {
             let connection;
             try{
                 connection = await mysql.createConnection(dbcreadentials);
@@ -77,37 +96,100 @@ app.get('/insert',
                 console.log('Connector: Got 2 Database!');
 
                 await connection.execute(`INSERT INTO ${TABLE} (${COLUMS}) VALUES (${VALUES})`);
-                console.log(`WRITER: Wrote new data to table: ${TABLE}, colum: ${COLUMS}, and wrote values: ${VALUES}!`);
+                console.log(`WRITER: Wrote new data to table: ${TABLE}, colum: ${COLUMS}, and wrote values: ${VALUES} on ${Time}!`);
 
-                res.json({
-                succes: true,
-                CODE: 200
+                res.status(202).json({
+                    succes: true,
+                    CODE: 202,
+                    Time: Time
                 });
                 console.log('STAT Data Send via json!');
                 console.log('SUCCES!');
                 console.log('EXIT Code 0');
             } catch(error) {
-                res.json({
+                res.status(500).json({
                     succes: false,
                     message: 'Internal Server ERROR',
-                    CODE: 500
+                    CODE: 500,
+                    Time: Time
                 });
                 console.log('Yup i failed');
                 console.log('EXIT Code 1');
+                console.log(error);
             } finally {
                 await connection.end();
             }
         } else {
-            res.json({
+            res.status(401).json({
                 succes: false,
-                message: 'Ey You need to enter a key!!!',
-                CODE: 401
+                message: 'Ey You got a licence for that?!!!',
+                CODE: 401,
+                Time: Time
             });
             console.log('Hey you got a alicense for that?');
             console.log('You newer take me alive "trumpet sound"');
         }
     }
 )
+
+app.get('/drop',
+    async (req, res) => {
+        console.log('[DROP] New request!')
+        const passhead = req.headers['pass'];
+        const password = req.query.pass;
+        const TABLE = req.query.t;
+        const IDDEL = req.query.id;
+        const Time = formatedDate();
+        if(AUTH(password,passhead) === true){
+            let connection;
+            try{
+                connection = await mysql.createConnection(dbcreadentials);
+                if(!TABLE){return};
+                if(!IDDEL){return};
+                console.log('Connector: Got 2 Database!');
+
+                await connection.execute(`DELETE FROM ${TABLE} WHERE id = ${IDDEL}`);
+                console.log(`Removed data from table ${TABLE} on id = ${IDDEL} on ${Time}`);
+
+                res.status(202).json({
+                    succes: true,
+                    CODE: 202,
+                    Time: Time
+                });
+                console.log('STAT Data Send via json!');
+                console.log('SUCCES!');
+                console.log('EXIT Code 0');
+            } catch (err) {
+                res.status(500).json({
+                    succes: false,
+                    message: 'Internal Server ERROR',
+                    CODE: 500,
+                    Time: Time
+                });
+
+                console.log('Yup i failed');
+                console.log('EXIT Code 1');
+                console.log(err);
+            } finally {
+                connection.end();
+            }
+        } else {
+            res.status(401).json({
+                succes: false,
+                message: 'Ey You got a licence for that?!!!',
+                CODE: 401,
+                Time: Time
+            });
+            console.log('Hey you got a alicense for that?');
+            console.log('You newer take me alive "trumpet sound"');
+        }
+    }
+)
+
+/*
+Syntax
+DELETE FROM nazov_tabulky WHERE id = 123;
+*/
 
 app.listen(PORT, () =>
     { console.log(`Status: API beží na porte: ${PORT}!`);
